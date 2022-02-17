9f253bef489b2ae87b2b87c31543cb3c1f6d976d
KhineZinThaw7 <scm.khinezinthaw@gmail.com>
2022-02-17 23:05:44 +0630

    make seeder and update readme

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AdminUser;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        AdminUser::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
